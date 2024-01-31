ae080f189dc6bdfdbed699d444b671e2e797ef29
Anna Lavrova <anna.laurier99@gmail.com>
2024-01-31 20:40:19 +0300

    kjb,jkhbkjhvjhgcjfcjgc jggkhkhg

--- HelloWorld/SceneDelegate.swift
+++ HelloWorld/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
@@ -30,7 +29,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
-
-
 }
-
