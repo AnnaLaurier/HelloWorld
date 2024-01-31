1ec0a4e7307446f0ce902ca7a2d80529d5c85986
Anna Lavrova <anna.laurier99@gmail.com>
2024-01-31 19:40:48 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Anna Lavrova on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let turnedOffAlpha = 0.3
+    private let turnedOnAlpha = 1.0
+
+    @IBOutlet private weak var redView: UIView!
+    @IBOutlet private weak var yellowView: UIView!
+    @IBOutlet private weak var greenView: UIView!
+    @IBOutlet private weak var startButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureButton(view: redView)
+        configureButton(view: yellowView)
+        configureButton(view: greenView)
+
+        startButton.layer.cornerRadius = 10
+        startButton.layer.cornerCurve = .continuous
+        startButton.setTitle("Start", for: .normal)
+    }
+
+    private func configureButton(view: UIView) {
+        view.layer.cornerRadius = view.frame.height / 2
+        view.alpha = turnedOffAlpha
+    }
+
+    @IBAction private func onStartButtonTapped() {
+        if redView.alpha == turnedOnAlpha {
+            yellowView.alpha = turnedOnAlpha
+            redView.alpha = turnedOffAlpha
+        } else if yellowView.alpha == turnedOnAlpha {
+            greenView.alpha = turnedOnAlpha
+            yellowView.alpha = turnedOffAlpha
+        } else if greenView.alpha == turnedOnAlpha {
+            redView.alpha = turnedOnAlpha
+            greenView.alpha = turnedOffAlpha
+        } else {
+            redView.alpha = turnedOnAlpha
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
+
+}
+
